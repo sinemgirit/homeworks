c93fe0f39b29a58f78543402998d2e36e33fe45e
Sinem Girit <sinemdursun106@gmail.com>
2024-03-05 22:59:47 +0300

    Do While , While Döngü Ödevi

---
+++ Homeworks/WhileDoWhile/Program.cs
@@ -0,0 +1,26 @@
+﻿
+//İkisinin de çalışma mantığı  aynıdır, ama while döngüsünde parantez içerisindeki koşul sağlandığı sürece kod bloğu çalışacaktır.
+// Do-while döngüsünde durum false olsa bile döngü en az bir kere çalışacaktır.
+
+
+
+// do while öreneği
+int girilenSayi = 0;
+
+do
+{
+	Console.WriteLine("0'dan büyük bir sayı giriniz: ");
+	girilenSayi = int.Parse(Console.ReadLine());
+	if (girilenSayi < 100)
+	{
+		Console.WriteLine("doğru sayı girdiniz!");	
+	}
+	else
+	{
+		Console.WriteLine("daha küçük sayı giriniz");	
+	}
+} while (girilenSayi > 100);
+
+
+
+
---
+++ Homeworks/WhileDongusu/Program.cs
@@ -0,0 +1,12 @@
+﻿// while örneği
+int number = 1;
+string name = ".sayı";
+while (true)
+{
+	if (number == 11)
+	{
+		break;
+	}
+	Console.WriteLine(number+ name);
+	number++;
+}
