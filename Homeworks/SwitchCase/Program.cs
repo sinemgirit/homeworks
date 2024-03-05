e229633f83e5a6e4a30d4819b8aae7ae62668ac4
Sinem Girit <sinemdursun106@gmail.com>
2024-03-05 20:00:20 +0300

    code tamamlandı

--- Homeworks/SwitchCase/Program.cs
+++ Homeworks/SwitchCase/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+
+
+SiparisNumarasıYaz();
+
+Console.WriteLine("Sipariş Numarası seçiniz ");
+string secim=Console.ReadLine();
+
+switch (secim)
+{
+	case "1":
+		Console.WriteLine("kalem ürününü seçtiniz");
+		break;
+	case "2":
+		Console.WriteLine("silgi ürününü seçtiniz");
+		break;
+	case "3":
+		Console.WriteLine("defter ürününü seçtiniz");
+		break;
+	case "4":
+		Console.WriteLine("Kalemlik ürününü seçtiniz");
+		break;
+
+	default:
+		break;
+}
+
+
+static void SiparisNumarasıYaz()
+
+{
+	Console.Clear();
+
+
+	Console.WriteLine("1) Kalem");
+	Console.WriteLine("2) Silgi");
+	Console.WriteLine("3) Defter");
+	Console.WriteLine("4) Kalemlik");
+	Console.WriteLine("0) Çıkış");
+	Console.WriteLine();
+
+}
+
+
+
