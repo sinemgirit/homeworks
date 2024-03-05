341fc4199dbf2212477b9caab00c1bbb5debcadb
Sinem Girit <sinemdursun106@gmail.com>
2024-03-06 00:35:55 +0300

    random sayı örneği

---
+++ Homeworks/RandomNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿Random rastgele = new Random();
+int sayi = rastgele.Next(1, 10);
+int number;
+do
+{
+	Console.WriteLine("1-10 arasında bir sayı giriniz");
+	number = Convert.ToInt32(Console.ReadLine());
+} while (number < 0 || number > 10);
+
+while (number != sayi)
+{
+	Console.WriteLine("Tekrar tahminde bulunun");
+	number = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("rastgele sayı" + number);
+Console.ReadKey();
