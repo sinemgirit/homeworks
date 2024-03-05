8952125519b1fae59a6f3e50fffe85b4ac1ab331
Sinem Girit <sinemdursun106@gmail.com>
2024-03-05 20:58:29 +0300

    for döngü ödevi

---
+++ Homeworks/ForDongusu/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Transactions;
+
+Console.Write("Kaç adet ürün istiyorsunuz:");
+Console.WriteLine();
+int piece=Convert.ToInt32(Console.ReadLine());
+double total = 0;
+
+for (int i = 0; i<piece; i++)
+{
+	
+	Console.Write("ürün fiyatı giriniz:");
+	Console.WriteLine();
+	double productprice = Convert.ToDouble(Console.ReadLine());
+	total=total+productprice;
+
+
+}
+Console.WriteLine("Ürnün Toplam Fiyatı:" + total);
+
+Console.ReadKey();
